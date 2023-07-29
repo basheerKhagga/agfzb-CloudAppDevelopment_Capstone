5a4351d3866b2d820f1aaedced68df9fb05bc07a
Muhammad Dawood <95972140+basheerKhagga@users.noreply.github.com>
2023-07-29 17:48:33 +0500

    Update main.py
    Updated with cf code

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,35 +1,33 @@
-"""IBM Cloud Function that gets all reviews for a dealership
-
-Returns:
-    List: List of reviews for the given dealership
-"""
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-import requests
-
-
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
+    authenticator = IAMAuthenticator(param_dict["IAM_API_KEY"])
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(param_dict["COUCH_URL"])
+    if 'dealerId' in param_dict:
+        response = service.post_find(db='reviews', selector={'dealership' : {'$eq' : int(param_dict['dealerId'])}},).get_result()
+        try:
+            result= {
+                'headers': {'Content-Type' : 'application/json'},
+                'body' : {'data':response}
+            }
+            return result
+        except:
+            return {
+                'statusCode' : 404,
+                'message': 'Something went wrong'
+            }
+    else:
+        response = service.post_document(db='reviews', document=param_dict["review"]).get_result()
+        try:
+            result= {
+            'headers': {'Content-Type':'application/json'},
+            'body': {'data':response}
+            }
+            return result
+        except:
+            return {
+            'statusCode': 404,
+            'message': 'Something went wrong'
+            }
